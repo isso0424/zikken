06b2b2bd83ce5bcd1e89cbde0600da452bab9619
isso0424 <isso04240129@gmail.com>
2020-10-08 14:50:21 +0900

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "string.h"
+#include "command_sorting.c"
+
+void interprinter() {
+  while (1) {
+    printf("Please Enter Command\nIf you don't know about commands, you can use `help`.\n>>>");
+    char command[256];
+    scanf("%s", &command);
+    printf("%s", command);
+    command_sorting(command);
+    if (!strcmp(command, "exit")) return;
+  }
+}
+
+int main() {
+  interprinter();
+}
+
