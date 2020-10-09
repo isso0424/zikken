b44611f71e4ff6c19c0c9a4653746663d8589912
isso0424 <isso04240129@gmail.com>
2020-10-09 15:29:56 +0900

    [Add]Created sort from name

--- command_sorting.c
+++ command_sorting.c
@@ -2,15 +2,49 @@
 #include "save.c"
 #include "load.c"
 
-int check_sorted(ROSTER rosters[], int rosters_size) {
+int check_sorted_with_name(ROSTER rosters[], int rosters_size) {
+  for (int i = 0; i < rosters_size - 1; i++) {
+    if (strcmp(rosters[i].name, rosters[i + 1].name) > 0) return 1;
+  }
+
+  return 0;
+}
+
+int sort_with_name(ROSTER rosters[], int rosters_size) {
+  while(check_sorted_with_name(rosters, rosters_size)) {
+    ROSTER tmp = rosters[0];
+    for (int i = 1; i < rosters_size; i++) {
+      if (strcmp(tmp.name, rosters[i].name) > 0) {
+        rosters[i - 1] = rosters[i];
+        rosters[i] = tmp;
+      }
+      tmp = rosters[i];
+    }
+  }
+
+  printf("No.|Name|Guraduated\n");
+  for (int i = 0; i < rosters_size; i++) {
+    printf(
+      "%d|%s|%s\n",
+      rosters[i].number,
+      rosters[i].name,
+      rosters[i].guraduated
+    );
+  }
+
+  return 0;
+}
+
+int check_sorted_with_number(ROSTER rosters[], int rosters_size) {
   for (int i = 0; i < rosters_size - 1; i++) {
     if (rosters[i].number > rosters[i + 1].number) return 1;
   }
 
   return 0;
 }
+
 int sort_with_number(ROSTER rosters[], int rosters_size) {
-  while(check_sorted(rosters, rosters_size)) {
+  while(check_sorted_with_number(rosters, rosters_size)) {
     ROSTER tmp = rosters[0];
     for (int i = 1; i < rosters_size; i++) {
       if (tmp.number > rosters[i].number) {
@@ -56,6 +90,20 @@ int command_sorting(char command[]) {
     load(rosters);
     int size = sizeof(rosters) / sizeof(*rosters);
     sort_with_number(rosters, size);
+  }
+  if (!strcmp(command, "sort_with_name")) {
+    const char *file_name = "rosters.csv";
+    FILE *fp;
+    fp = fopen(file_name, "r");
+    if (fp == NULL) {
+      printf("Cannot open rosters.csv.\nPlease check if it exist.");
+      return 1;
+    }
+    int columns = get_file_columns(fp);
+    ROSTER rosters[columns];
+    load(rosters);
+    int size = sizeof(rosters) / sizeof(*rosters);
+    sort_with_name(rosters, size);
   }
 
   return 0;
